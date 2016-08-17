0e9c91b914658ffd43e42bab0a05cbaeb915aa47
Ryan Prichard <ryan.prichard@gmail.com>
2016-08-17 05:27:04 -0500

    Switch to using tar instead of zip.
    Cygwin unzip marks binaries as not-executable if they come from a ZIP file.
    
    Always use Cygwin to create the tar to ensure that the binaries are marked
    executable in the tar file.

--- dist/make-bin-package.bat
+++ dist/make-bin-package.bat
@@ -13,11 +13,14 @@ exit /b 1
 :: main routine
 
 :main
+setlocal
+
 set Unix2Dos=C:\cygwin64\bin\unix2dos.exe
 set ProjectRoot=%~dp0..
-set ProjectRootMix=%ProjectRoot:\=/%
+set ProjectRootM=%ProjectRoot:\=/%
 cd "%ProjectRoot%"
 rmdir /s/q out 2>NUL
+mkdir out\packages || exit /b 1
 set /p Version=<VERSION.txt
 
 "%WINDIR%\System32\bash.exe" -c "make -C backend && strip out/wslbridge-backend"    || exit /b 1
@@ -36,15 +39,17 @@ exit /b 0
 :: makeFrontend subroutine
 
 :makeFrontend
+setlocal
 
 set PackageName=wslbridge-%Version%-%1
 set PackageDir=out\%PackageName%
+set PackageDirM=out/%PackageName%
 mkdir %PackageDir% || exit /b 1
 
 del out\wslbridge.exe 2>NUL
-%2\bash.exe -l -c "cd '%ProjectRootMix%' && make -C frontend && strip out/wslbridge.exe"            || exit /b 1
-%2\bash.exe -l -c "cd '%ProjectRootMix%' && g++ --version > out/CygGccVersion.txt"                  || exit /b 1
-%Unix2Dos% out/CygGccVersion.txt                                                                    || exit /b 1
+%2\bash.exe -l -c "cd '%ProjectRootM%' && make -C frontend && strip out/wslbridge.exe"  || exit /b 1
+%2\bash.exe -l -c "cd '%ProjectRootM%' && g++ --version > out/CygGccVersion.txt"        || exit /b 1
+%Unix2Dos% out/CygGccVersion.txt                                                        || exit /b 1
 
 copy out\wslbridge.exe     %PackageDir% || exit /b 1
 copy out\wslbridge-backend %PackageDir% || exit /b 1
@@ -57,7 +62,15 @@ type out\CygDllVersion.txt      >> %PackageDir%\BuildInfo.txt   || exit /b 1
 type out\CygGccVersion.txt      >> %PackageDir%\BuildInfo.txt   || exit /b 1
 type out\WslGccVersion.txt      >> %PackageDir%\BuildInfo.txt   || exit /b 1
 
-del %PackageDir%.zip 2>NUL
-"C:\Program Files\7-Zip\7z.exe" a %PackageDir%.zip .\%PackageDir%\* || exit /b 1
+:: Always use Cygwin tar to package the binary.  The MSYS2 tar will mark the
+:: backend executable because its POSIX emulation uses filetype to determine
+:: executability.  Cygwin, on the other hand, uses NTFS ACL entries.  If the
+:: MSYS2 package were then extracted using Cygwin tar, the backend file would
+:: have non-executable ACL entries, and WSL would refuse to run it.
+set DistSavedPath=%PATH%
+set PATH=C:\cygwin64\bin;%PATH%
+C:\cygwin64\bin\chmod.exe -x %PackageDirM%/BuildInfo.txt || exit /b 1
+C:\cygwin64\bin\tar.exe cfz out/packages/%PackageName%.tar.gz --numeric-owner --owner=0 --group=0 -C out %PackageName% || exit /b 1
+set PATH=%DistSavedPath%
 
 exit /b 0
