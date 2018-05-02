51e9f9f840158056761230e92763a1b97f8b7821
Ryan Prichard <ryan.prichard@gmail.com>
2018-05-02 02:10:25 -0700

    Update Cygwin/MSYS2 prebuilts and add a rebaseall step
    Hopefully it fixes this build failure:
    https://ci.appveyor.com/project/rprichard/wslbridge/build/1.0.15/job/irotx0hwe0nuogll

--- dist/make-frontend-packages.py
+++ dist/make-frontend-packages.py
@@ -22,13 +22,13 @@ mkdirs(artifactDir)
 
 os.chdir(buildDir)
 
-baseUrl = 'https://ci.appveyor.com/api/buildjobs/1h4j8i22owwhybjf/artifacts/out%2Fartifact%2F'
+baseUrl = 'https://ci.appveyor.com/api/buildjobs/1qy7umu9penjvdsv/artifacts/out%2Fartifact%2F'
 
 for platform, binDir, url in [
-            ('cygwin32', 'bin',     baseUrl + 'cygwin32-20180501-dll2.10.0-gcc6.4.0.7z'),
-            ('cygwin64', 'bin',     baseUrl + 'cygwin64-20180501-dll2.10.0-gcc6.4.0.7z'),
-            ('msys32',   'usr/bin', baseUrl + 'msys32-20180501-dll2.10.0-gcc7.3.0.7z'),
-            ('msys64',   'usr/bin', baseUrl + 'msys64-20180501-dll2.10.0-gcc7.3.0.7z'),
+            ('cygwin32', 'bin',     baseUrl + 'cygwin32-20180502-dll2.10.0-gcc6.4.0.7z'),
+            ('cygwin64', 'bin',     baseUrl + 'cygwin64-20180502-dll2.10.0-gcc6.4.0.7z'),
+            ('msys32',   'usr/bin', baseUrl + 'msys32-20180502-dll2.10.0-gcc7.3.0.7z'),
+            ('msys64',   'usr/bin', baseUrl + 'msys64-20180502-dll2.10.0-gcc7.3.0.7z'),
         ]:
 
     if os.getenv('CYGWIN_VARIANT') and os.getenv('CYGWIN_VARIANT') != platform:
@@ -44,6 +44,8 @@ for platform, binDir, url in [
 
     origPATH = os.getenv('PATH')
     os.putenv('PATH', platformBinDir + os.pathsep + origPATH)
+    # Rebase the binaries after installing/moving them onto the new machine.
+    check_call([os.path.join(platformBinDir, 'ash.exe'), '/{}/rebaseall'.format(binDir), '-v'])
     check_call([os.path.join(platformBinDir, 'make.exe'), 'clean'], cwd=os.path.join(projectDir, 'frontend'))
     check_call([os.path.join(platformBinDir, 'make.exe')],          cwd=os.path.join(projectDir, 'frontend'))
     check_call([os.path.join(platformBinDir, 'tar.exe'), 'cfa',
